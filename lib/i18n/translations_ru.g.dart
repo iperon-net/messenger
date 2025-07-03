50368a5f2255b553af46c8ea50784fdffab1d6b1
Kostya Ten <kostya@yandex.ru>
2025-07-03 16:55:34 +0300

    feat(i18n): restructure and enhance Russian translations
    Restructured Russian translations with new categories and subcategories for better clarity and extensibility. Added missing translations and refined existing ones.

--- lib/i18n/translations_ru.g.dart
+++ lib/i18n/translations_ru.g.dart
@@ -40,37 +40,106 @@ class TranslationsRu extends Translations {
 
 	// Translations
 	@override String get notImplementedScreen => 'Не реализованный экран';
-	@override String get contacts => 'Контакты';
-	@override String get chats => 'Чаты';
+	@override late final _TranslationsUiRu ui = _TranslationsUiRu._(_root);
+	@override late final _TranslationsDrawerRu drawer = _TranslationsDrawerRu._(_root);
+	@override late final _TranslationsContactsRu contacts = _TranslationsContactsRu._(_root);
+	@override late final _TranslationsChatsRu chats = _TranslationsChatsRu._(_root);
+	@override late final _TranslationsSettingsRu settings = _TranslationsSettingsRu._(_root);
+}
+
+// Path: ui
+class _TranslationsUiRu extends TranslationsUiEn {
+	_TranslationsUiRu._(TranslationsRu root) : this._root = root, super.internal(root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
 	@override String get settings => 'Настройки';
-	@override String get language => 'Язык';
+}
+
+// Path: drawer
+class _TranslationsDrawerRu extends TranslationsDrawerEn {
+	_TranslationsDrawerRu._(TranslationsRu root) : this._root = root, super.internal(root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get settings => _root.ui.settings;
 	@override String get favorites => 'Избранное';
-	@override String get appearance => 'Оформление';
-	@override String get myProfile => 'Мой профиль';
-	@override String get myPersonalInfo => 'Моя личная информация';
-	@override String get versionApplication => 'Версия приложения';
-	@override String get privacyAndSecurity => 'Конфиденциальность';
-	@override String get devices => 'Устройства';
-	@override String get updates => 'Обновления';
-	@override String get interfaceLanguage => 'Язык интерфейса';
-	@override String get sort => 'Сортировка';
+	@override String get contacts => 'Контакты';
+}
+
+// Path: contacts
+class _TranslationsContactsRu extends TranslationsContactsEn {
+	_TranslationsContactsRu._(TranslationsRu root) : this._root = root, super.internal(root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get contacts => 'Контакты';
 	@override String get search => 'Поиск';
-	@override String get favorite_contacts => 'Избранные контакты';
-	@override String get back => 'Назад';
-	@override String get theme => 'Тема';
-	@override String get themeSystem => 'Системная';
-	@override String get themeLight => 'Светлая';
-	@override String get themeDark => 'Темная';
-	@override String get online => 'В сети';
-	@override String last_seen_minutes({required num n}) => (_root.$meta.cardinalResolver ?? PluralResolvers.cardinal('ru'))(n,
-		one: 'был(а) ${n} минуту назад',
-		few: 'был(а) ${n} минуты назад',
-		many: 'был(а) ${n} минут назад',
-		other: 'был(а) ${n} минут назад',
-	);
-	@override String get waiting_for_network => 'Ожидание сети...';
-	@override String get all_contacts => 'Все контакты';
+	@override late final _TranslationsContactsTabsRu tabs = _TranslationsContactsTabsRu._(_root);
+}
+
+// Path: chats
+class _TranslationsChatsRu extends TranslationsChatsEn {
+	_TranslationsChatsRu._(TranslationsRu root) : this._root = root, super.internal(root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get chats => 'Чаты';
+}
+
+// Path: settings
+class _TranslationsSettingsRu extends TranslationsSettingsEn {
+	_TranslationsSettingsRu._(TranslationsRu root) : this._root = root, super.internal(root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get settings => 'Настройки';
+	@override String get favorites => 'Избранное';
+	@override String get myPersonalInfo => 'Моя личная информация';
+	@override String get myProfile => 'Мой профиль';
+	@override String get privacyAndSecurity => 'Конфиденциальность';
+	@override String get devices => 'Девайсы';
+	@override String get updates => 'Обновления';
+	@override late final _TranslationsSettingsAppearanceRu appearance = _TranslationsSettingsAppearanceRu._(_root);
+	@override late final _TranslationsSettingsLanguageRu language = _TranslationsSettingsLanguageRu._(_root);
+}
+
+// Path: contacts.tabs
+class _TranslationsContactsTabsRu extends TranslationsContactsTabsEn {
+	_TranslationsContactsTabsRu._(TranslationsRu root) : this._root = root, super.internal(root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
 	@override String get all => 'Все';
+	@override String get favorites => 'Избранное';
+}
+
+// Path: settings.appearance
+class _TranslationsSettingsAppearanceRu extends TranslationsSettingsAppearanceEn {
+	_TranslationsSettingsAppearanceRu._(TranslationsRu root) : this._root = root, super.internal(root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get appearance => 'Оформление';
+}
+
+// Path: settings.language
+class _TranslationsSettingsLanguageRu extends TranslationsSettingsLanguageEn {
+	_TranslationsSettingsLanguageRu._(TranslationsRu root) : this._root = root, super.internal(root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get language => 'Язык';
+	@override String get back => 'Назад';
+	@override String get interfaceLanguage => 'Язык интерфейса';
 }
 
 /// Flat map(s) containing all translations.
@@ -79,37 +148,26 @@ extension on TranslationsRu {
 	dynamic _flatMapFunction(String path) {
 		switch (path) {
 			case 'notImplementedScreen': return 'Не реализованный экран';
-			case 'contacts': return 'Контакты';
-			case 'chats': return 'Чаты';
-			case 'settings': return 'Настройки';
-			case 'language': return 'Язык';
-			case 'favorites': return 'Избранное';
-			case 'appearance': return 'Оформление';
-			case 'myProfile': return 'Мой профиль';
-			case 'myPersonalInfo': return 'Моя личная информация';
-			case 'versionApplication': return 'Версия приложения';
-			case 'privacyAndSecurity': return 'Конфиденциальность';
-			case 'devices': return 'Устройства';
-			case 'updates': return 'Обновления';
-			case 'interfaceLanguage': return 'Язык интерфейса';
-			case 'sort': return 'Сортировка';
-			case 'search': return 'Поиск';
-			case 'favorite_contacts': return 'Избранные контакты';
-			case 'back': return 'Назад';
-			case 'theme': return 'Тема';
-			case 'themeSystem': return 'Системная';
-			case 'themeLight': return 'Светлая';
-			case 'themeDark': return 'Темная';
-			case 'online': return 'В сети';
-			case 'last_seen_minutes': return ({required num n}) => (_root.$meta.cardinalResolver ?? PluralResolvers.cardinal('ru'))(n,
-				one: 'был(а) ${n} минуту назад',
-				few: 'был(а) ${n} минуты назад',
-				many: 'был(а) ${n} минут назад',
-				other: 'был(а) ${n} минут назад',
-			);
-			case 'waiting_for_network': return 'Ожидание сети...';
-			case 'all_contacts': return 'Все контакты';
-			case 'all': return 'Все';
+			case 'ui.settings': return 'Настройки';
+			case 'drawer.settings': return _root.ui.settings;
+			case 'drawer.favorites': return 'Избранное';
+			case 'drawer.contacts': return 'Контакты';
+			case 'contacts.contacts': return 'Контакты';
+			case 'contacts.search': return 'Поиск';
+			case 'contacts.tabs.all': return 'Все';
+			case 'contacts.tabs.favorites': return 'Избранное';
+			case 'chats.chats': return 'Чаты';
+			case 'settings.settings': return 'Настройки';
+			case 'settings.favorites': return 'Избранное';
+			case 'settings.myPersonalInfo': return 'Моя личная информация';
+			case 'settings.myProfile': return 'Мой профиль';
+			case 'settings.privacyAndSecurity': return 'Конфиденциальность';
+			case 'settings.devices': return 'Девайсы';
+			case 'settings.updates': return 'Обновления';
+			case 'settings.appearance.appearance': return 'Оформление';
+			case 'settings.language.language': return 'Язык';
+			case 'settings.language.back': return 'Назад';
+			case 'settings.language.interfaceLanguage': return 'Язык интерфейса';
 			default: return null;
 		}
 	}
