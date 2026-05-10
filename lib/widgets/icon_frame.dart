17fefe16c2270daf2e3bf3e5f380cc546ab8b637
Kostya Ten <kostya@yandex.ru>
2026-05-10 14:18:05 +0300

    feat(widgets): add `IconFrameWidget` and refactor `DividerTextWidget` as reusable components
    - Introduced `IconFrameWidget` for consistent icon rendering with configurable size, color, and border radius.
    - Refactored `dividerText` into a stateless `DividerTextWidget` for improved reusability.
    - Updated imports and refactored `auth` and `settings` screens to utilize the new widgets.
    
    Signed-off-by: Kostya Ten <kostya@yandex.ru>

--- lib/widgets/icon_frame.dart
+++ lib/widgets/icon_frame.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
 
 class IconFrameWidget extends StatelessWidget{
   final double width;
