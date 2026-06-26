8703c591c6c70ef28d3d4e31aa413d69e3dfd6ce
Kostya Ten <kostya@yandex.ru>
2026-06-27 01:00:32 +0300

    refactor(home): remove unused imports from `HomeCubit`
    Signed-off-by: Kostya Ten <kostya@yandex.ru>

--- lib/screens/home/home_cubit.dart
+++ lib/screens/home/home_cubit.dart
@@ -1,8 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:yandex_login_sdk/yandex_login_sdk.dart';
 
-import '../../di.dart';
-import '../../logger.dart';
 import 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
