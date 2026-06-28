cd3d8b1212ed2effe3a68ec5c49e163f4f956c1a
Kostya Ten <kostya@yandex.ru>
2026-06-28 14:39:51 +0300

    refactor(settings): clean up unused imports and UI-related methods in device sessions screen
    Signed-off-by: Kostya Ten <kostya@yandex.ru>

--- lib/screens/settings/settings_device_sessions_screen_cupertino.dart
+++ lib/screens/settings/settings_device_sessions_screen_cupertino.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:messenger/cubit/main_cubit.dart';
-import 'package:messenger/cubit/settings/settings_cubit.dart';
 import 'package:messenger/cubit/settings/settings_device_sessions_state.dart';
-import 'package:messenger/cubit/settings/settings_language_state.dart';
 
 import '../../cubit/settings/settings_device_sessions_cubit.dart';
 import '../../i18n/translations.g.dart';
-import '../../cubit/settings/settings_language_cubit.dart';
 
 
 class SettingsDeviceSessionsScreenCupertino extends StatefulWidget {
@@ -29,22 +25,6 @@ class _SettingsDeviceSessionsScreenCupertino extends State<SettingsDeviceSession
     super.dispose();
   }
 
-  Widget _divider() {
-    return Container(
-      color: CupertinoDynamicColor.resolve(
-        CupertinoDynamicColor.withBrightness(
-          color: CupertinoTheme.of(context).scaffoldBackgroundColor,
-          darkColor: const Color(0xFF1C1C1E),
-        ),
-        context,
-      ),
-      child: Container(
-        height: 0.3,
-        color: CupertinoDynamicColor.resolve(CupertinoColors.separator, context),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<SettingsDeviceSessionsCubit, SettingsDeviceSessionsState>(
@@ -55,60 +35,66 @@ class _SettingsDeviceSessionsScreenCupertino extends State<SettingsDeviceSession
       },
       builder: (context, state) {
         return CupertinoPageScaffold(
-          backgroundColor: CupertinoDynamicColor.withBrightness(
-            color: CupertinoColors.systemGroupedBackground,
-            darkColor: CupertinoColors.darkBackgroundGray,
-          ),
+          backgroundColor: CupertinoColors.systemGroupedBackground,
           navigationBar: CupertinoNavigationBar(
             automaticBackgroundVisibility: false,
             middle: Text(context.t.devices),
           ),
           child: SafeArea(
-            child: Center(
-              child: ListView(
-                children: [
-                  CupertinoListSection.insetGrouped(
-                    header: Text(
-                      "Это устройство",
-                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-                    ),
-                    footer: Text(
-                      "Выйти на всех устройствах, кроме текущего",
-                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-                    ),
-                    children: [
-
-                      CupertinoListTile(
-                        padding: EdgeInsets.all(10),
-                        leading: Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF1755DC),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Center(
-                            child: FaIcon(FontAwesomeIcons.apple, size: 18, color: Color(0xFFFFFFFF)),
-                          ),
-                        ),
-                        title: Column(
-                          spacing: 4,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Iphone 16 Pro"),
-                            Text(
-                              "iOS  26.5",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ],
-                        ),
-                        subtitle: Text('Россия, Москва • в сети'),
-                      ),
-                    ],
+            child: ListView(
+              children: [
+                CupertinoListSection.insetGrouped(
+                  header: Text(
+                    "Это устройство",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
                   ),
+                  footer: Text(
+                    "Выйти на всех устройствах, кроме текущего",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+                  ),
+                  children: [
 
-                ],
-              ),
+                    CupertinoListTile(
+                      padding: EdgeInsets.all(10),
+                      leading: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF1755DC),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Center(
+                          child: FaIcon(FontAwesomeIcons.apple, size: 18, color: Color(0xFFFFFFFF)),
+                        ),
+                      ),
+                      title: Column(
+                        spacing: 4,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Iphone 16 Pro"),
+                          Text(
+                            "iOS  26.5",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      subtitle: Text('Россия, Москва • вчера в 12:00'),
+                    ),
+                    CupertinoListTile(
+                      padding: EdgeInsets.all(10),
+                      leading: FaIcon(FontAwesomeIcons.hand, size: 18, color: Color(0xFFF40000)),
+                      title: Text(
+                        "Завершить другие сеансы",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: CupertinoColors.destructiveRed,
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         );
